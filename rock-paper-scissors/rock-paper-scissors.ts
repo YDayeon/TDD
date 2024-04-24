d15e2f81d279c79259286fcb8d462f8a40ce09c2
Dayeon Yoon <yday1223@gmail.com>
2024-04-24 10:48:26 +0900

    refactor: rock-paper-scissors

--- rock-paper-scissors/rock-paper-scissors.ts
+++ rock-paper-scissors/rock-paper-scissors.ts
@@ -12,20 +12,46 @@ interface RockScissorsPaper {
   play(playerMove: Move, opponent: Move): Outcome;
 }
 export function createRockPaperScissors(): RockScissorsPaper {
+  const scenarios = [
+    {
+      playerMove: Move.Paper,
+      opponentMove: Move.Rock,
+      outcome: Outcome.PlayerWins,
+    },
+    {
+      playerMove: Move.Paper,
+      opponentMove: Move.Scissors,
+      outcome: Outcome.PlayerLoses,
+    },
+    {
+      playerMove: Move.Rock,
+      opponentMove: Move.Scissors,
+      outcome: Outcome.PlayerWins,
+    },
+    {
+      playerMove: Move.Rock,
+      opponentMove: Move.Paper,
+      outcome: Outcome.PlayerLoses,
+    },
+    {
+      playerMove: Move.Scissors,
+      opponentMove: Move.Paper,
+      outcome: Outcome.PlayerWins,
+    },
+    {
+      playerMove: Move.Scissors,
+      opponentMove: Move.Rock,
+      outcome: Outcome.PlayerLoses,
+    },
+  ];
+
   const play = (playerMove: Move, opponentMove: Move) => {
-    if (
-      (playerMove === Move.Paper && opponentMove === Move.Rock) ||
-      (playerMove === Move.Rock && opponentMove === Move.Scissors) ||
-      (playerMove === Move.Scissors && opponentMove === Move.Paper)
-    )
-      return Outcome.PlayerWins;
-    if (
-      (playerMove === Move.Paper && opponentMove === Move.Scissors) ||
-      (playerMove === Move.Rock && opponentMove === Move.Paper) ||
-      (playerMove === Move.Scissors && opponentMove === Move.Rock)
-    )
-      return Outcome.PlayerLoses;
-    return Outcome.Tie;
+    const result = scenarios.find(
+      (scenario) =>
+        scenario.playerMove === playerMove &&
+        scenario.opponentMove === opponentMove
+    );
+    return result ? result.outcome : Outcome.Tie;
   };
   return { play };
 }
